baf5cfc8e4403b080808afe94ab90fce7fff1c81
Ubuntu <amitpurohit@amit-dev-jenkins.ut2vue5pauyeta5wcbbnusiaud.zx.internal.cloudapp.net>
2021-11-27 15:04:39 +0000

    fixing frontend

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "Deploy stage"
+
+scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
+
+ssh jenkins@swarm-manager \
+    DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+    DATABASE_URI=$DATABASE_RI \
+    docker stack deploy --compose-file docker-compose.yaml todo-app
