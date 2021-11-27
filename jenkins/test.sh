177b811a5d9a72548ca574657999ca9d711f647b
Ubuntu <amitpurohit@amit-dev-jenkins.ut2vue5pauyeta5wcbbnusiaud.zx.internal.cloudapp.net>
2021-11-27 16:00:23 +0000

    adding test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "Test stage 2"
+source venv/bin/activate
+python3 -m pytest \
+--cov=application \
+--cov-report term-missing \
+--cov-report xml:coverage.xml \
+--junitxml=junit_report.xml
