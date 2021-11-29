244ab6c54f8a83f316b10b03532520ffa55a7b7b
Ubuntu <amitpurohit@amit-dev-jenkins.ut2vue5pauyeta5wcbbnusiaud.zx.internal.cloudapp.net>
2021-11-29 12:33:59 +0000

    fixing bugs

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -6,5 +6,5 @@ scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 
 ssh jenkins@swarm-manager \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
-    DATABASE_URI=$DATABASE_RI \
+    DATABASE_URI=$DATABASE_URI \
     docker stack deploy --compose-file docker-compose.yaml todo-app
